6085eb74533c901b62dc14089e7c6ab3c7cd1f6f
shinmiji <911024s@naver.com>
2015-09-30 10:50:00 +0900

    gallery
    gallery version 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,82 @@
-var button_list = document.querySelectorAll('.button_area > a');
-var button_list_leng = button_list.length;
-var business = document.querySelectorAll('ul > li');
-var business_leng = business.length;
-var business_ul = document.querySelector('ul');
-var business_li_size = 407;
 
-// ul의 width값을 li의 갯수를 고려하여 동적으로 할당.
-business_ul.style['width'] = business_li_size*business.length+'px';
+;(function(global, $, undefined){
+	'use strict';
 
-// li안에 있는 a태그에 onclick 이벤트 설정.
-for (var i=button_list_leng-1 ; i >-1; i--) {
-	var button = button_list[i];
-	button.onclick = buttonController.bind(button);
-}
+	var $business = $('.business > ul');
+	var $business_contents = $('li', $business);
 
-// click 하였을 때 실행되는 함수
-function buttonController() {
-	for (var i = 0 ; i < button_list_leng; i++) {
-		var class_list = button_list[i].getAttribute('class');
-		var _class_list = (class_list || ' ').split(' ');
+	var $button = $('.button_area > a');
+	var $target = $button.first();
 
-		for (var j = _class_list.length-1 ;j >-1 ; j--) {
-			if (_class_list[j] === 'on') {
-				button_list[i].classList.remove('on');
-			}	
+	var view_size = parseInt($('.business').css('width'));
+	var business_contents_size = parseInt($business_contents.css('width'));
+	var currentIndex = 1;
+	var timeID = -1;
+
+
+	// ul의 width값을 li의 갯수를 고려하여 동적으로 할당.
+	$business.css('width', business_contents_size*$business_contents.length+'px');
+
+
+	initFunction();
+
+	function initFunction() {
+
+		// slide auto play
+		slideAutoPlay('start');
+
+		// 버튼에 onclick 이벤트 설정.
+		$button.on('click', function(){
+			buttonClickEvent.call(this, event);
+		});
+
+		// 갤러리에 마우스 올릴 경우 슬라이드 정지
+		$business.on('mouseenter', function() {
+			slideAutoPlay('stop');
+		});
+
+		// 갤러리에서 마우스 벗어날 경우 슬라이드 재생
+		$business.on('mouseleave', function() {
+			slideAutoPlay('start');
+		});
+
+	};
+
+	// slide auto play
+	function slideAutoPlay(action) {
+		if(action === 'start') {
+			timeID = setInterval(function(){
+					$business.animate(
+						{'margin-left': '-'+view_size*currentIndex+'px'},
+						'slow'
+					);
+					$target.removeClass('on');
+					$target = $button.eq(currentIndex);
+					$target.addClass('on');
+
+					if(currentIndex === 3) {
+						currentIndex =0;
+					}
+					else {
+						currentIndex++;
+					}
+				}, 3000);
+		} else {
+			clearInterval(timeID);
 		}
-		this.classList.add('on');
-	}	
-
-	var on_class = this.getAttribute('class');
 	
-	var _on_class_ = on_class.split(' ');
+	};
 
-	for (var i = _on_class_.length-1 ; i >-1; i--) {
-		switch(_on_class_[i]) {
-			case 'btn1':
-				business_ul.style['left'] = business_li_size*(-0)+'px';
-				break;
-			case 'btn2':
-				business_ul.style['left'] = business_li_size*(-3)+'px';
-				break;
-			case 'btn3':
-				business_ul.style['left'] = business_li_size*(-6)+'px';
-				break;
-			case 'btn4':
-				business_ul.style['left'] = business_li_size*(-9)+'px';
-				break;
-		}
-	}
-	return false;
-}
+	// 버튼 클릭 시 슬라이드 이동
+	function buttonClickEvent(event) {
+		event.preventDefault();
+		$target.removeClass('on');
+		$target = $(this);
+		$target.addClass('on');
+		currentIndex = $target.index();
+		$business.animate(
+			{'margin-left': '-'+view_size*currentIndex+'px'},
+			'slow'
+		);
+	};
+})(window, window.jQuery);
